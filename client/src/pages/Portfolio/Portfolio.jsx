f354371e152df4b0c08634064047af16ec6e912e
Code Green <nmacharashvili171993@gmail.com>
2025-03-07 04:43:17 +0400

    boom

--- client/src/pages/Portfolio/Portfolio.jsx
+++ client/src/pages/Portfolio/Portfolio.jsx
@@ -11,12 +11,14 @@ const Portfolio = () => {
           <img className="portfolio-img " src={yourCodeImg} alt="hello" />
           <div className="portfolio-text-container">
             <h1>Who I Am!</h1>
-            <span className="portfolio-text">
-              Hello, my name is Nikoloz Macharashvili. I have been developing
-              User Interfaces for over 4 years with React and Vue. I know SASS,
-              Tailwind, BootStrap, MUI on the frontend. And TypeScript, node,
-              mongodb on the backend.
-            </span>
+            <div className="portfolio-text-main">
+              <span className="portfolio-text">
+                Hello, my name is Nikoloz Macharashvili. I have been developing
+                User Interfaces for over 4 years with React and Vue. I know
+                SASS, Tailwind, BootStrap, MUI on the frontend. And TypeScript,
+                node, mongodb on the backend.
+              </span>
+            </div>
             <div className="center left">
               <Link className="link-btn" to="/projects">
                 <span className="text">Check Out My Projects</span>
